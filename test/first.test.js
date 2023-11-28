684ff1ec423077c002e2276d8e8bb6ffd0aca6ed
shivganesh <shivmarkad99@gmail.com>
2023-11-28 11:47:58 +0530

    [Shivganesh] Add: testing using mocha and chai

---
+++ test/first.test.js
@@ -0,0 +1,64 @@
+const chai = require('chai')
+
+const assert = chai.assert
+const should = chai.should()
+const expect = chai.expect
+
+// ***************** assert *************
+
+describe('---Check Assert---',function(){
+    let userName = 'Shiv';
+
+    it('check type of variable--',function(){
+        assert.typeOf(userName,'string');
+    })
+    it('check value of variable--',function(){
+        assert.equal(userName,'Shiv');
+    })
+    it('check length of variable--',function(){
+        assert.lengthOf(userName,4);
+    })
+})
+
+
+// ***************** Should *************
+
+describe('---Check should---',function(){
+    let userName = 'Shiv';
+
+    it('should check type of variable--',function(){
+        userName.should.be.a('string')
+    })
+    it('should check value of variable--',function(){
+        userName.should.equal('Shiv')
+    })
+    it('should check length of variable--',function(){
+        userName.should.have.lengthOf(4)
+    })
+})
+
+
+// ***************** expect *************
+
+
+
+describe('---Expect Check ---',function(){
+    let userName = 'Shiv';
+    let details = {
+        age: 24,
+        role: 'DevOps Engg'
+    }
+    it('Expect check type of variable--',function(){
+        expect(userName).be.a('string')
+    })
+    it('Expect check value of variable--',function(){
+        expect(userName).equal('Shiv')
+    })
+    it('Expect check length of variable--',function(){
+        expect(userName).have.lengthOf(4)
+    })
+
+    it('Expect check property of object--',function(){
+        expect(details).haveOwnProperty('age')
+    })
+})
